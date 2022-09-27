30be558c330c1382da51886cb074bc38d8ec5743
Kirill-Kondratev <kondratevk.1989@mail.ru>
2022-09-28 01:14:19 +0700

    Add error boundary

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,18 @@
+import React, { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
+
+export const AppRouter = () => (
+	<Suspense fallback={<PageLoader />}>
+		<Routes>
+			{Object.values(routeConfig).map(({ element, path }) => (
+				<Route
+					key={path}
+					path={path}
+					element={(<div className="page-wrapper">{element}</div>)}
+				/>
+			))}
+		</Routes>
+	</Suspense>
+);
