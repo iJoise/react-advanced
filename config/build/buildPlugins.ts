8abf09c279931a6a59c2eb19160d34c591ce529e
Kirill-Kondratev <kondratevk.1989@mail.ru>
2022-10-07 02:04:59 +0700

    Add github ci

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -9,7 +9,7 @@ export function buildPlugins({
 	paths,
 	isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-	return [
+	const plugins = [
 		new HTMLWebpackPlugin({
 			template: paths.html,
 		}),
@@ -21,11 +21,16 @@ export function buildPlugins({
 		new webpack.DefinePlugin({
 			__IS_DEV__: JSON.stringify(isDev),
 		}),
-		new ReactRefreshWebpackPlugin({
-			overlay: false,
-		}),
-		new BundleAnalyzerPlugin({
-			openAnalyzer: false,
-		}),
 	];
+
+	if (isDev) {
+		plugins.push(new ReactRefreshWebpackPlugin({
+			overlay: false,
+		}));
+		plugins.push(new BundleAnalyzerPlugin({
+			openAnalyzer: false,
+		}));
+	}
+
+	return plugins;
 }
