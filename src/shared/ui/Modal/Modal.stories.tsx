282d74c23334463c7a7d2f4ce24b76794946e53a
ijoise <kondratevk.1989@mail.ru>
2022-10-21 02:41:29 +0700

    fix tests

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -24,6 +24,7 @@ Primary.args = {
 export const Dark = Template.bind({});
 Dark.args = {
 	isOpen: true,
+	className: 'app_dark_theme',
 	children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi '
 		+ 'consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
