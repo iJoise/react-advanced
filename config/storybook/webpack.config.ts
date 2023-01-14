d48a821ef908f1bf2a484825e449ac7d02c96eb0
Kirill Kondratev <kondratevk.1989@mail.ru>
2023-01-14 15:45:41 +0300

    fix storybook webpack config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,49 +1,64 @@
-import webpack, { Configuration, DefinePlugin, RuleSetRule } from 'webpack';
 import path from 'path';
-import { BuildPaths } from '../build/types/config';
+import type { RuleSetRule, Configuration, WebpackPluginInstance } from 'webpack';
+import { DefinePlugin } from 'webpack';
+import type { BuildPaths } from '../build/types/config';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 
-export default ({ config }: { config: webpack.Configuration }) => {
-	const paths: BuildPaths = {
-		build: '',
-		html: '',
-		entry: '',
-		src: path.resolve(__dirname, '..', '..', 'src'),
-	};
-
-	return {
-		...config,
-		module: {
-			rules: config.module.rules.map((rule: RuleSetRule) => {
-				if (/svg/.test(rule.test as string)) {
-					return {
-						...rule,
-						exclude: /\.svg$/i,
-					};
-				}
-
-				return rule;
-			})
-				.concat({
-					test: /\.svg$/,
-					issuer: /\.[jt]sx?$/,
-					use: ['@svgr/webpack'],
-				})
-				.concat({
-					...buildCssLoader(true),
-				}),
-		},
-		plugins: [
-			...config.plugins,
-			new DefinePlugin({
-				__IS_DEV__: true,
-				__API__: '',
-			}),
-		],
-		resolve: {
-			...config.resolve,
-			modules: [paths.src, 'node_modules'],
-			extensions: [...config.resolve.extensions, '.tsx', '.ts'],
-		},
-	} as Configuration;
+const buildPath: BuildPaths = {
+	entry: '',
+	build: '',
+	html: '',
+	src: path.resolve(__dirname, '..', '..', 'src'),
 };
+
+interface Resolve {
+	modules: string[];
+	extensions: string[];
+}
+
+interface Module {
+	rules: RuleSetRule[];
+}
+
+interface Config extends Configuration {
+	plugins: WebpackPluginInstance[];
+	resolve: Resolve;
+	module: Module;
+}
+
+export default ({ config }: { config: Config }) => ({
+	...config,
+	module: {
+		rules: config.module.rules.map((rule: RuleSetRule) => {
+			if (RegExp(rule.test as RegExp)
+				.test('.svg')) {
+				return {
+					...rule,
+					exclude: /\.svg$/i,
+				};
+			}
+
+			return rule;
+		})
+			.concat({
+				test: /\.svg$/i,
+				issuer: /\.[jt]sx?$/,
+				use: ['@svgr/webpack'],
+			})
+			.concat({
+				...buildCssLoader(true),
+			}),
+	},
+	plugins: [
+		...config.plugins,
+		new DefinePlugin({
+			__IS_DEV__: JSON.stringify(true),
+			__API__: JSON.stringify(''),
+		}),
+	],
+	resolve: {
+		...config.resolve,
+		modules: [buildPath.src, ...config.resolve.modules],
+		extensions: [...config.resolve.extensions, '.tsx', '.ts'],
+	},
+} as Configuration);
